ece6e2fb32dbf8c4b4a58a077d199ab5bfeba128
PKKMarek <p.k.k.marek@gmail.com>
2022-11-17 13:02:38 +0100

    Fajnie

--- views.py
+++ views.py
@@ -5,3 +5,15 @@ views = Blueprint(__name__,"views")
 @views.route("/")
 def home():
     return render_template("index.html")
+
+@views.route("/NasiOpiekunowie")
+def NasiOpiekunowie():
+    return render_template("indexNasiOpiekunowie.html")
+
+@views.route("/NaszePsiaki")
+def NaszePsiaki():
+    return render_template("indexNaszePsiaki.html")
+
+@views.route("/Kontakt")
+def Kontakt():
+    return render_template("indexKontakt.html")
